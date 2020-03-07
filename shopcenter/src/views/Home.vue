0365fa52bed12400be21d26ec2259dfc7cfac063
tanghao1194 <1136656317@qq.com>
2020-03-07 22:36:06 +0800

    03-07 first

---
+++ shopcenter/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <el-button type='primary'>测试按钮</el-button>
+    <el-row>
+        <el-col :span = 8> <el-button type = 'primary'>按钮左</el-button></el-col>
+        <el-col :span = 16><el-button type = 'success' @click="tonewpage">按钮右</el-button></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+    
+  },
+  methods:{
+   tonewpage(){
+       this.$router.push('./new');
+   }
+  }
+}
+</script>
---
+++ shopcenter/src/views/New.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        new页面
+
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>>
